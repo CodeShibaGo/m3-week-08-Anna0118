af637ebde0b1068948be3a0744e80a3200676091
Anna0118 <annachu0988@gmail.com>
2024-03-26 10:57:04 +0800

    add youtube Data Api

--- youtube/script.js
+++ youtube/script.js
@@ -1,54 +1,54 @@
-// addEventListener
+// 搜索框監聽
 document.getElementById("search-form").addEventListener("submit", function (e) {
-  e.preventDefault(); // 防止表單跳轉和重新刷新頁面
-  search(); // 調用自定義的 search 函數來處理表單提交
+  e.preventDefault();
+  const q = document.getElementById("query").value;
+  loadVideos("", q);
 });
 
-//Search
-function search() {
-  var resultsElement = document.getElementById("results");
-  var buttonsElement = document.getElementById("buttons");
+// 上/下頁按鈕監聽
+document.getElementById("buttons").addEventListener("click", function (e) {
+  if (e.target.classList.contains("paging-button")) {
+    e.preventDefault();
+    const token = e.target.getAttribute("data-token");
+    const q = e.target.getAttribute("data-query");
+    loadVideos(token, q);
+  }
+});
 
-  // Clear previous results
-  resultsElement.innerHTML = "";
-  buttonsElement.innerHTML = "";
-
-  // Get Form Input
-  var q = document.getElementById("query").value;
-
-  var apiKey = "AIzaSyAGw2Lgj16eu4IpoyY1U47zbiashZRy58c";
-  var requestURL = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&type=video&q="+
-  ${q} + &key=${apiKey}`;
+// Load search lists
+function loadVideos(token = "") {
+  const q = document.getElementById("query").value;
+  const apiKey = "AIzaSyA_PGKxKojyjZLjb_tNUV9pLq6DgwtGcSI"; // 因為目前demo是丟到github page，目前沒查詢到保護的機制
+  const requestURL =
+    `https://www.googleapis.com/youtube/v3/search?part=snippet,id&type=video&q=${q}&key=${apiKey}` +
+    (token ? `&pageToken=${token}` : "");
 
   fetch(requestURL)
     .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
+      if (!response.ok) throw new Error("Network response was not ok");
       return response.json();
     })
     .then((data) => {
-      console.log(data); // Console log for debugging
-      var nextPageToken = data.nextPageToken;
-      var prevPageToken = data.prevPageToken;
+      const resultsElement = document.getElementById("results");
+      const buttonsElement = document.getElementById("buttons");
+      resultsElement.innerHTML = ""; // Clear previous results
+      buttonsElement.innerHTML = ""; // Clear previous results
 
-      // Dynamically create list items for each video and append to results element
-      data.items.forEach((item) => {
-        var output = getOutput(item); // getOutput is a function that returns HTML string
-        var li = document.createElement("li");
-        li.innerHTML = output;
-        resultsElement.appendChild(li);
-      });
-
-      var buttons = getButtons(prevPageToken, nextPageToken); // returns HTML string for buttons
-      buttonsElement.innerHTML = buttons;
+      data.items.forEach(
+        (item) => (resultsElement.innerHTML += getOutput(item))
+      );
+      buttonsElement.innerHTML = getButtons(
+        data.prevPageToken,
+        data.nextPageToken,
+        q
+      ); // 將q傳遞到下一個page
     })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+    .catch((error) =>
+      console.error("There was a problem with the fetch operation:", error)
+    );
 }
 
-// Build videoo lists Output
+// Build html Output
 function getOutput(item) {
   var videoId = item.id.videoId;
   var title = item.snippet.title;
@@ -88,120 +88,17 @@ function getOutput(item) {
 }
 
 // Build the buttons
-function getButtons(prevPageToken, nextPageToken) {
-  if (!prevPageToken) {
-    var btnoutput =
-      '<div class="button-container">' +
-      '<button id="next-button" class="paging-button" data-token="' +
-      nextPageToken +
-      '" data-query="' +
-      q +
-      '"' +
-      'onclick="nextPage();">Next Page</button></div>';
-  } else {
-    var btnoutput =
-      '<div class="button-container">' +
-      '<button id="prev-button" class="paging-button" data-token="' +
-      prevPageToken +
-      '" data-query="' +
-      q +
-      '"' +
-      'onclick="prevPage();">Prev Page</button>' +
-      '<button id="next-button" class="paging-button" data-token="' +
-      nextPageToken +
-      '" data-query="' +
-      q +
-      '"' +
-      'onclick="nextPage();">Next Page</button></div>';
+function getButtons(prevPageToken, nextPageToken, query) {
+  let buttonsHtml = '<div class="button-container">';
+
+  if (prevPageToken) {
+    buttonsHtml += `<button id="prev-button" class="paging-button" data-token="${prevPageToken}" data-query="${query}">Prev Page</button>`;
   }
 
-  return btnoutput;
-}
+  if (nextPageToken) {
+    buttonsHtml += `<button id="next-button" class="paging-button" data-token="${nextPageToken}" data-query="${query}">Next Page</button>`;
+  }
 
-// Next Page Function
-function nextPage() {
-  var token = document.getElementById("next-button").getAttribute("data-token");
-  //   var q = document.getElementById("query").value;
-
-  // Clear previous results
-  resultsElement.innerHTML = "";
-  buttonsElement.innerHTML = "";
-
-  // Get Form Input
-  var q = document.getElementById("query").value;
-
-  var apiKey = "AIzaSyAGw2Lgj16eu4IpoyY1U47zbiashZRy58c";
-  var requestURL = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&type=video&q="+
-  ${q} + &key=${apiKey} + pageToken=${token}`;
-
-  fetch(requestURL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data); // Console log for debugging
-      var nextPageToken = data.nextPageToken;
-      var prevPageToken = data.prevPageToken;
-
-      // Dynamically create list items for each video and append to results element
-      data.items.forEach((item) => {
-        var output = getOutput(item); // getOutput is a function that returns HTML string
-        var li = document.createElement("li");
-        li.innerHTML = output;
-        resultsElement.appendChild(li);
-      });
-
-      var buttons = getButtons(prevPageToken, nextPageToken); // returns HTML string for buttons
-      buttonsElement.innerHTML = buttons;
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
-}
-
-// Prev Page Function
-function nextPage() {
-  var token = document.getElementById("prev-button").getAttribute("data-token");
-  //   var q = document.getElementById("query").value;
-
-  // Clear previous results
-  resultsElement.innerHTML = "";
-  buttonsElement.innerHTML = "";
-
-  // Get Form Input
-  var q = document.getElementById("query").value;
-
-  var apiKey = "AIzaSyAGw2Lgj16eu4IpoyY1U47zbiashZRy58c";
-  var requestURL = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&type=video&q="+
-  ${q} + &key=${apiKey} + pageToken=${token}`;
-
-  fetch(requestURL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data); // Console log for debugging
-      var nextPageToken = data.nextPageToken;
-      var prevPageToken = data.prevPageToken;
-
-      // Dynamically create list items for each video and append to results element
-      data.items.forEach((item) => {
-        var output = getOutput(item); // getOutput is a function that returns HTML string
-        var li = document.createElement("li");
-        li.innerHTML = output;
-        resultsElement.appendChild(li);
-      });
-
-      var buttons = getButtons(prevPageToken, nextPageToken); // returns HTML string for buttons
-      buttonsElement.innerHTML = buttons;
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+  buttonsHtml += "</div>";
+  return buttonsHtml;
 }
